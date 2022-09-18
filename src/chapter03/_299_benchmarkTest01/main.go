e994e1b0153146eaef23599cfdd4a9fefbfad222
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-18 11:13:29 +0800

    AM 01st commit

---
+++ src/chapter03/_299_benchmarkTest01/main.go
@@ -0,0 +1,10 @@
+package _299_benchmarkTest01
+
+/**
+*Description==>TODO
+*BelongsProject==>goProject
+*BelongsPackage==>
+*CreateTime==>2022-09-18 11:13:11
+*Version==>1.0
+*Author==>02雪乃赤瞳楪祈校条祭制作委员会 wyq_start
+ */
