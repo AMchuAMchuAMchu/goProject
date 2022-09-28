e1142047a9efa8a807e81ab04641960ef4aa8dc4
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-28 16:16:39 +0800

    AM 01st commit

--- src/chapter03/_99尚硅谷_流程控制课后练习说明/main/main.go
+++ src/chapter03/_99尚硅谷_流程控制课后练习说明/main/main.go
@@ -11,10 +11,18 @@ import "fmt"
 func main() {
 
 	for i := 100; i <= 1000; i++ {
+
 		n1 := i / 100
 		n2 := i % 100 / 10
 		n3 := i % 10
 		if n1*n1*n1+n2*n2*n2+n3*n3*n3 == i {
+			fmt.Println(i)
+		}
+
+		n4 := i / 100
+		n5 := i % 100 / 10
+		n6 := i % 10
+		if n4*n4*n4+n5*n5*n5+n6*n6*n6 == i {
 			fmt.Println(i)
 		}
 
