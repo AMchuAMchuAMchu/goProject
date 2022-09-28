4c52dce2808d794c108a1965d81e7826df8ce81c
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-28 18:54:47 +0800

    AM 01st commit

--- src/chapter03/_301_demo/main/main.go
+++ src/chapter03/_301_demo/main/main.go
@@ -8,3 +8,18 @@ package main
  * CreateTime ==> 2022-09-28 18:33:25
  * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
  */
+
+/**
+* Description ==> TODO
+* Return Type ==>
+* Params args ==>
+* Create Time ==> 2022/9/28 18:48
+* author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_start
+ */
+func add(num01 int, num02 int) int {
+	return num01 + num02
+}
+
+func main() {
+
+}
