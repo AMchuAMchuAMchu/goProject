80a20e4eb7ea80c445fa8530203295b6a2e30be2
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-28 18:36:20 +0800

    AM 01st commit

---
+++ src/chapter03/_301_demo/main/main.go
@@ -0,0 +1,10 @@
+package main
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> goProject
+ * BelongsPackage ==>
+ * Version ==> 1.0
+ * CreateTime ==> 2022-09-28 18:33:25
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
